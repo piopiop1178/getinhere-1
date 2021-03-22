c18413f54f1c88f1650939294ce95afaa1984853
Ubuntu <ubuntu@ip-172-31-7-244.ap-northeast-2.compute.internal>
2021-03-22 12:49:08 +0000

    init

--- server/RoomManager.js
+++ server/RoomManager.js
@@ -20,7 +20,7 @@ class RoomManager{   // Room 함수 실행
         const tokens = uuid.v4().split('-');
         const roomName = tokens[2] + tokens[1] + tokens[0] + tokens[3] + tokens[4];
         console.log("********** createRoom ***********");
-        console.log(`localhost?room=${roomName}`);
+        console.log(`https://3.34.42.125?room=${roomName}`);
         /* roomName으로 새로운 Room을 생성하여 rooms에 추가 */
         const room = new Room(roomName, map);
         this.roomByName[roomName] = room;
