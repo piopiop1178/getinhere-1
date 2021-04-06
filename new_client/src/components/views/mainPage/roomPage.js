efe108ed81ad670c165ca724b8efc499ad626835
piopiop <piopiop1178@gmail.com>
2021-04-06 12:32:00 +0000

    device loaded debug

--- new_client/src/components/views/mainPage/roomPage.js
+++ new_client/src/components/views/mainPage/roomPage.js
@@ -598,7 +598,7 @@ class Room extends Component {
 
         socket.on('createScreenShareConsumer', async (socketId, audio) => {
             // let sameSpace = (users[socketId].space === curr_space) ? true : false
-            while (!recvTransport || !device || !document.getElementById(socketId || !device.loaded)) {
+            while (!recvTransport || !device || !document.getElementById(socketId) || !device.loaded) {
                 await this.sleep(100);
         }
 
