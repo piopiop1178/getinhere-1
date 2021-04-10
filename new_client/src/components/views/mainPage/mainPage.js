b716c86b6d18b3f1e4ed8f6a0b8c6e3d39bd98bc
gojae <kjhchs41@gmail.com>
2021-04-10 17:15:43 +0900

    microsoft browser block

--- new_client/src/components/views/mainPage/mainPage.js
+++ new_client/src/components/views/mainPage/mainPage.js
@@ -68,7 +68,10 @@ class Mainpage extends Component {
         let filter = "win16|win32|win64|mac|macintel";
         if ( navigator.platform ) {
             if ( filter.indexOf( navigator.platform.toLowerCase() ) < 0 
-              || navigator.userAgent.toLowerCase().indexOf("chrome") === -1) {
+              || navigator.userAgent.toLowerCase().indexOf("chrome") === -1
+              || (navigator.appName === 'Netscape' && agent.indexOf('trident') !== -1)
+              || agent.indexOf("msie") !== -1
+              ){
                 //mobile || notChrome
                 window.location.href="/"
             }
